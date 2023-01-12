bb583dafa42054ee55131da992e1f8bb6ea4a66d
CMSuisse <cmarti@gmx.net>
2023-01-12 14:34:30 +0100

    First steps in website development

---
+++ Dreisatz/script.js
@@ -0,0 +1,10 @@
+function calculate(){
+    var number_1 = document.getElementById("number-1").value;
+    var number_2 = document.getElementById("number-2").value;
+    var number_3 = document.getElementById("number-3").value;
+    var solution = document.getElementById("solution");
+
+    var relation_number_1_to_number_2 = number_2/number_1;
+    var solution_number = number_3 * relation_number_1_to_number_2;
+    solution.value = solution_number;
+}
